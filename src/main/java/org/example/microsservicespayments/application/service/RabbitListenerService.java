a498240f10ed0a75f9780486c26135fcff1c909e
pauloalarico <paulocraft20@gmail.com>
2026-01-25 05:48:13 -0300

    feat(rabbit) start the configuration of the rabbit consumer of payments.

---
+++ src/main/java/org/example/microsservicespayments/application/service/RabbitListenerService.java
@@ -0,0 +1,12 @@
+package org.example.microsservicespayments.application.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class RabbitListenerService {
+    private final PaymentService service;
+}
---
+++ src/main/java/org/example/microsservicespayments/configuration/RabbitConfig.java
@@ -0,0 +1,57 @@
+package org.example.microsservicespayments.configuration;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public Queue createOrdered() {
+        return new Queue("orders-payed.queue", false);
+    }
+
+    @Bean
+    public RabbitAdmin createAdmin(ConnectionFactory factory) {
+        return new RabbitAdmin(factory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initializeRabbit(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public FanoutExchange createExchange() {
+        return new FanoutExchange("orders-payed.ex");
+    }
+
+    @Bean
+    public Binding createOrderBinding(FanoutExchange exchange) {
+        return BindingBuilder.bind(createOrdered()).to(exchange);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter());
+        return factory;
+    }
+}
